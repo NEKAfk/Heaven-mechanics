f265d21b7b37cd737dbd79956e915e997b323a2e
NEKAfk <nikitazavalov994@gmail.com>
2020-10-24 14:58:21 +0400

    Created main script with examlple

---
+++ main.py
@@ -0,0 +1,88 @@
+import pygame as pg
+import math as m
+from random import randint
+
+RES = WIDTH, HEIGHT = 1300, 700
+FPS = 120
+T = 0.5
+planets = list()
+
+pg.init()
+done = False
+surface = pg.display.set_mode(RES, pg.RESIZABLE)
+clock = pg.time.Clock()
+
+
+def event_overview(events):
+    global done
+    for event in events:
+        if event.type == pg.QUIT:
+            done = True
+
+
+class HeavenBody:
+    standard_radius = 5
+
+    def __init__(self, mass, x, y, V, ang):
+        self.m = mass
+        self.x, self.y = x, y
+        self.Vx, self.Vy = V * m.cos(ang / 180 * m.pi), V * m.sin(ang / 180 * m.pi)
+        self.r = HeavenBody.standard_radius
+        self.tracer = [(0, 0) for i in range(250)]
+        self.tracer[0] = (x, y)
+        self.it = 1
+        self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+    def move(self, p, lenght):
+        global T
+        acceleration = a(p.m, lenght) * T
+        k = lenght / acceleration
+        self.Vx += (p.x - self.x) / k
+        self.Vy += (p.y - self.y) / k
+        self.x += self.Vx * T
+        self.y += self.Vy * T
+
+    def draw(self):
+        for x, y in self.tracer:
+            pg.draw.circle(surface, (255 - self.color[0], 255 - self.color[1], 255 - self.color[2]), (x, y), 1)
+        pg.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.r)
+        self.tracer[self.it] = (int(self.x), int(self.y))
+        if self.it == 249:
+            self.it = -1
+        self.it += 1
+
+
+def a(mass, lenght):
+    return mass / lenght ** 2
+
+
+def r(p1, p2):
+    return (abs(p1.x - p2.x) ** 2 + abs(p1.y - p2.y) ** 2) ** 0.5
+
+def calculation_and_drawing(planets):
+    for i in range(-1, len(planets) - 1):
+        distance = r(planets[i], planets[i + 1])
+        planets[i].move(planets[i + 1], distance)
+        planets[i + 1].move(planets[i], distance)
+    for planet in planets:
+        planet.draw()
+
+def main_loop():
+    global done
+    pl1 = HeavenBody(5000, WIDTH // 2, HEIGHT // 2, 0, 0)
+    pl2 = HeavenBody(1, WIDTH // 2, HEIGHT - 200, 3, 0)
+    pl3 = HeavenBody(50, WIDTH // 2, 200, 2, 0)
+    planets.append(pl1)
+    planets.append(pl2)
+    planets.append(pl3)
+    while not done:
+        surface.fill(pg.Color("black"))
+        event_overview(pg.event.get())
+        calculation_and_drawing(planets)
+        # pg.display.set_caption(str(distance))
+        pg.display.flip()
+        clock.tick(FPS)
+
+
+if __name__ == "__main__":
+    main_loop()
