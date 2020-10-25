f0c522504615b15e1cff0ab60440c0feb0194a21
NEKAfk <nikitazavalov994@gmail.com>
2020-10-25 10:26:02 +0400

    Setting some parametres

--- main.py
+++ main.py
@@ -40,14 +40,22 @@ class Body(ABC):
 
 
 class MovementBody(Body):
-    standard_radius = 5
+    standard_radius = 10
 
     def __init__(self, mass, x, y, V=0.0, ang=0, r=standard_radius):
         super(MovementBody, self).__init__(mass, x, y, r)
         self.Vx, self.Vy = V * m.cos(ang / 180 * m.pi), V * m.sin(ang / 180 * m.pi)
-        self.tracer = [(0, 0) for i in range(1001)]
+        self.tracer = [(0, 0) for i in range(501)]
         self.tracer[0] = (x, y)
         self.it = 1
+
+    def __add__(self, other):
+        self.r = (self.r**2 + other.r**2)**0.5
+        self.m += other.m
+        if type(other) == MovementBody:
+            self.Vx += other.Vx * self.m / other.m
+            self.Vy += other.Vy * self.m / other.m
+        return self
 
     def move(self, p=None, lenght=0):
         acceleration = a(p.m, lenght)
@@ -60,9 +68,9 @@ class MovementBody(Body):
     def draw(self):
         for x, y in self.tracer:
             pg.draw.circle(surface, (255 - self.color[0], 255 - self.color[1], 255 - self.color[2]), (x, y), 1)
-        pg.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.r)
+        pg.draw.circle(surface, self.color, (int(self.x), int(self.y)), int(self.r))
         self.tracer[self.it] = (int(self.x), int(self.y))
-        if self.it == 1000:
+        if self.it == 500:
             self.it = -1
         self.it += 1
 
@@ -89,11 +97,19 @@ def r(p1, p2):
 
 
 def calculation_and_drawing(planets):
-    for i in range(-1, len(planets) - 1):
-        if type(planets[i]) == StaticBody:
-            continue
-        distance = r(planets[i], planets[i + 1])
-        planets[i].move(planets[i + 1], distance)
+    for p1 in planets:
+        for p2 in planets[0:planets.index(p1)] + planets[planets.index(p1) + 1:]:
+            try:
+                if type(p1) == StaticBody:
+                    break
+                distance = r(p1, p2)
+                if distance < 30:
+                    planets[planets.index(p1)] = p1 + p2
+                    planets.remove(p2)
+                    continue
+                p1.move(p2, distance)
+            except Exception:
+                break
     for planet in planets:
         planet.draw()
 
@@ -101,7 +117,8 @@ def calculation_and_drawing(planets):
 def main_loop():
     global done
     planets.append(StaticBody(333, WIDTH // 2, HEIGHT // 2))
-    planets.append(MovementBody(1, WIDTH // 2, HEIGHT // 2 + 150, 3.5, 0))
+    planets.append(MovementBody(1, WIDTH // 2, HEIGHT // 2 + 50, 5, 0))
+    planets.append(MovementBody(10, WIDTH // 2 - 200, HEIGHT // 2, 2, 90))
     while not done:
         surface.fill(pg.Color("black"))
         event_overview(pg.event.get())
